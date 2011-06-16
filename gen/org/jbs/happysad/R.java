a72092d01cb59fb4d4a46d60fff1f8689b3614a3
Jared Dunn <jwdunn1@brandeis.edu>
2011-06-16 12:30:03 -0400

    commit with faces

--- gen/org/jbs/happysad/R.java
+++ gen/org/jbs/happysad/R.java
@@ -11,22 +11,28 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int happysad=0x7f020000;
-        public static final int icon=0x7f020001;
+        public static final int frown=0x7f020000;
+        public static final int happysad=0x7f020001;
+        public static final int icon=0x7f020002;
+        public static final int smile=0x7f020003;
     }
     public static final class id {
-        public static final int happy_button=0x7f050002;
-        public static final int more_text=0x7f050004;
-        public static final int more_textbox=0x7f050005;
-        public static final int more_to_dash=0x7f050006;
-        public static final int sad_button=0x7f050003;
+        public static final int happy_button=0x7f050006;
+        public static final int linearLayout1=0x7f050003;
+        public static final int more_text=0x7f050007;
+        public static final int more_textbox=0x7f050008;
+        public static final int more_to_dash=0x7f050009;
+        public static final int relativeLayout1=0x7f050004;
+        public static final int sad_button=0x7f050005;
+        public static final int text=0x7f050002;
         public static final int update_button=0x7f050001;
         public static final int welcome_title=0x7f050000;
     }
     public static final class layout {
         public static final int dashboard=0x7f030000;
-        public static final int main=0x7f030001;
-        public static final int more=0x7f030002;
+        public static final int history=0x7f030001;
+        public static final int main=0x7f030002;
+        public static final int more=0x7f030003;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
@@ -34,6 +40,8 @@ public final class R {
         public static final int happy_prompt=0x7f040002;
         public static final int happysad_name=0x7f040004;
         public static final int hello=0x7f040000;
+        public static final int im_happy=0x7f04000b;
+        public static final int im_sad=0x7f04000c;
         public static final int more_hint_text=0x7f040008;
         public static final int more_name=0x7f040007;
         public static final int more_question=0x7f040005;
