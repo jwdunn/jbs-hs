5021e59e8556e8f3fabe0797ce67fe16516eb23f
ttkirkland <ttkirkland@gmail.com>
2011-06-26 23:31:41 -0400

    cleaner code

--- src/org/jbs/happysad/HappyDB.java
+++ src/org/jbs/happysad/HappyDB.java
@@ -20,10 +20,6 @@ import static org.jbs.happysad.Constants.UID;
 public class HappyDB extends SQLiteOpenHelper {
 	private static final String DATABASE_NAME = "happy.db";
 	private static final int DATABASE_VERSION = 2;
-	private static final String TAG = "HappyDB"; 
-
-	
-	
 	
 	/**
 	 * Constructor for the DB object
