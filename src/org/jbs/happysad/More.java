f80b51191132971bc803672ea1b6ac66459d07bf
ttkirkland <ttkirkland@gmail.com>
2011-07-05 15:24:36 -0400

    gps on onResume/gps off on onPause

--- src/org/jbs/happysad/More.java
+++ src/org/jbs/happysad/More.java
@@ -83,8 +83,7 @@ public class More extends Activity implements OnClickListener, OnTouchListener, 
 		//prevent text edit from being focused onCreate
 		getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
 		
-		//Updates location
-		locationStuff();
+	
 		
 		//Finds the submit_button view
 		View submitButton = findViewById(R.id.more_to_dash);
@@ -132,10 +131,6 @@ public class More extends Activity implements OnClickListener, OnTouchListener, 
 			Intent i = new Intent(this, Dashboard.class);
 			String userstring = ((TextView) findViewById(R.id.more_textbox)).getText().toString();
 			saveUpdate(userstring); 
-			gpsLocationManager.removeUpdates(gpsLocationListener);
-			networkLocationManager.removeUpdates(networkLocationListener);
-			gpsLocationManager = null;
-			networkLocationManager = null;
 			finish();
 			startActivity(i);
 			break;
@@ -321,39 +316,53 @@ public class More extends Activity implements OnClickListener, OnTouchListener, 
 
 		}
 
-		/** Show an event in the LogCat view, for debugging */
-		private void dumpEvent(MotionEvent event) {
-			String names[] = { "DOWN" , "UP" , "MOVE" , "CANCEL" , "OUTSIDE" ,
-					"POINTER_DOWN" , "POINTER_UP" , "7?" , "8?" , "9?" };
-			StringBuilder sb = new StringBuilder();
-			int action = event.getAction();
-			int actionCode = action & MotionEvent.ACTION_MASK;
-			sb.append("event ACTION_" ).append(names[actionCode]);
-			if (actionCode == MotionEvent.ACTION_POINTER_DOWN || actionCode == MotionEvent.ACTION_POINTER_UP) {
-				sb.append("(pid " ).append(action >> MotionEvent.ACTION_POINTER_ID_SHIFT);
+	/** Show an event in the LogCat view, for debugging */
+	private void dumpEvent(MotionEvent event) {
+		String names[] = { "DOWN" , "UP" , "MOVE" , "CANCEL" , "OUTSIDE" ,
+				"POINTER_DOWN" , "POINTER_UP" , "7?" , "8?" , "9?" };
+		StringBuilder sb = new StringBuilder();
+		int action = event.getAction();
+		int actionCode = action & MotionEvent.ACTION_MASK;
+		sb.append("event ACTION_" ).append(names[actionCode]);
+		if (actionCode == MotionEvent.ACTION_POINTER_DOWN || actionCode == MotionEvent.ACTION_POINTER_UP) {
+			sb.append("(pid " ).append(action >> MotionEvent.ACTION_POINTER_ID_SHIFT);
 				sb.append(")" );
 			}
-			sb.append("[" );
-			for (int i = 0; i < event.getPointerCount(); i++) {
-				sb.append("#" ).append(i);
-				sb.append("(pid " ).append(event.getPointerId(i));
-				sb.append(")=" ).append((int) event.getX(i));
-				sb.append("," ).append((int) event.getY(i));
-				if (i + 1 < event.getPointerCount())
-					sb.append(";" );
-			}
-			sb.append("]" );
-			}
-
-		private float spacing(MotionEvent event) {
-			float x = event.getX(0) - event.getX(1);
-			float y = event.getY(0) - event.getY(1);
-			return FloatMath.sqrt(x * x + y * y);
-			}
-		private void midPoint(PointF point, MotionEvent event) {
-			float x = event.getX(0) + event.getX(1);
-			float y = event.getY(0) + event.getY(1);
-			point.set(x / 2, y / 2);
+		sb.append("[" );
+		for (int i = 0; i < event.getPointerCount(); i++) {
+			sb.append("#" ).append(i);
+			sb.append("(pid " ).append(event.getPointerId(i));
+			sb.append(")=" ).append((int) event.getX(i));
+			sb.append("," ).append((int) event.getY(i));
+			if (i + 1 < event.getPointerCount())
+				sb.append(";" );
 		}
+		sb.append("]" );
+	}
+
+	private float spacing(MotionEvent event) {
+		float x = event.getX(0) - event.getX(1);
+		float y = event.getY(0) - event.getY(1);
+		return FloatMath.sqrt(x * x + y * y);
+	}
+	
+	private void midPoint(PointF point, MotionEvent event) {
+		float x = event.getX(0) + event.getX(1);
+		float y = event.getY(0) + event.getY(1);
+		point.set(x / 2, y / 2);
+	}
+		
+	public void onPause() {
+		super.onPause();
+		gpsLocationManager.removeUpdates(gpsLocationListener);
+		networkLocationManager.removeUpdates(networkLocationListener);
+		gpsLocationManager = null;
+		networkLocationManager = null;
+	}
+	
+	public void onResume() {
+		super.onResume();
+		locationStuff();
+	}
 }
 
