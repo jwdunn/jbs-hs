27d7bae22668f29f50d1e1105f93a3440c5a465d
ttkirkland <ttkirkland@gmail.com>
2011-07-06 14:40:43 -0400

    prettier history screen

--- src/org/jbs/happysad/History.java
+++ src/org/jbs/happysad/History.java
@@ -1,9 +1,15 @@
 package org.jbs.happysad;
 
+import java.io.IOException;
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+
 import android.app.ListActivity;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -20,6 +26,7 @@ public class History extends ListActivity implements OnClickListener{
 	private static int[] TO = {R.id.item_text1, R.id.item_text2 };
 	private static String[] FROM = { "line1","line2" };	
 	private SimpleAdapter adapter;
+	StringBuilder result;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState){
@@ -50,14 +57,12 @@ public class History extends ListActivity implements OnClickListener{
 		for (HappyBottle b : l){
 			Log.d("History", "adding bottle with msg " + b.getMsg());
 			HashMap<String, String> m = new HashMap<String, String>();
-			String e = (b.getEmo()>.5)?"Happy":"Sad"; //notice this assumes that emo is still a float. We will change this later.
-			Log.d(TAG, "time:"+new Timestamp(b.getTime()).toString()  );
-			Log.d(TAG, "other:"+e + " : " +  b.getMsg());
-			m.put("line1", new Timestamp(b.getTime()).toString() );
-			m.put("line2", e + " : " +  b.getMsg());
-			//m.put(TIME, new Timestamp(b.getTime()).toString());
-			//m.put(MSG, b.getMsg());
-			//m.put(EMO, e);
+			String e = (b.getEmo()>0)?"Happy":"Sad";
+			//Double lat = (double) b.getLat();
+			//Double longi = (double) b.getLong();
+			//getAddress(lat, longi);
+			m.put("line1", e + " : " +  b.getMsg());
+			m.put("line2", new Timestamp(b.getTime()).toLocaleString() );
 			
 			newList.add(m);
 		}
@@ -65,5 +70,30 @@ public class History extends ListActivity implements OnClickListener{
 		setListAdapter(adapter);
 		
 	}
+	/*
+	public void getAddress(double lat, double longi){
+        try{
+            Geocoder gcd = new Geocoder(this, Locale.getDefault());
+            List<Address> addresses = 
+                gcd.getFromLocation(lat, longi,100);
+            if (addresses.size() > 0) {
+                result = new StringBuilder();
+                for(int i = 0; i < addresses.size(); i++){
+                    Address address =  addresses.get(i);
+                    int maxIndex = address.getMaxAddressLineIndex();
+                    for (int x = 0; x <= maxIndex; x++ ){
+                        result.append(address.getAddressLine(x));
+                        result.append(",");
+                    }               
+                    result.append(address.getLocality());
+                    result.append(",");
+                    result.append(address.getPostalCode());
+                    result.append("\n\n");
+                }
+            }
+        }
+        catch(IOException ex){
+        }
+    } */
 	
 }
