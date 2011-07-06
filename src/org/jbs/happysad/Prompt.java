d8d9b7178c15afbef42854336e2a9438e13f4b63
Jared Dunn <jwdunn1@brandeis.edu>
2011-07-06 19:38:25 -0400

    this is with the userid;s added also a background picture on the prompt

--- src/org/jbs/happysad/Prompt.java
+++ src/org/jbs/happysad/Prompt.java
@@ -1,11 +1,16 @@
 package org.jbs.happysad;
 
 
+import android.accounts.Account;
+import android.accounts.AccountManager;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Toast;
 
 /**
  * Creates Prompt Activity
@@ -16,6 +21,7 @@ public class Prompt extends Activity implements OnClickListener{
 	private Syncer s;
 	private Thread t;
 	private int myID = 1;
+	public static final String USER_DATA = "userdata";
 	/**
 	 * Initializes activity
 	 */
@@ -36,7 +42,35 @@ public class Prompt extends Activity implements OnClickListener{
     	t = new Thread(s);
     	t.start();
     	
+    	final AccountManager manager = AccountManager.get(this);
+	    final Account[] accounts = manager.getAccounts();   
+	    
+            if (accounts.length >=1){
+            	String username = new String(accounts[0].name);
+            	int UID = userNameHash(username);
+            	SharedPreferences sp = getSharedPreferences(USER_DATA,0);
+            	SharedPreferences.Editor editor = sp.edit();
+            	editor.putInt("usernameint", UID);
+            	editor.putString("usernameString", username); 
+            	editor.commit();
+            	
+            	Context context = getApplicationContext();
+            	String loginPrompt = getString(R.string.login_message);
+                CharSequence loggedin = (loginPrompt +" " + sp.getString( "usernameString", ""));
+            	int duration = Toast.LENGTH_LONG;
+            	Toast toast = Toast.makeText(context, loggedin, duration);
+            	toast.show();
+            }
+            else{
+            	Context context = getApplicationContext();
+            	String loginPrompt = getString(R.string.error_login_message);
+            	CharSequence text = loginPrompt;
+            	int duration = Toast.LENGTH_LONG;
+            	Toast toast = Toast.makeText(context, text, duration);
+            	toast.show();
+            }
     }
+           
     
     /**
      * Invoked when a view is clicked
@@ -61,4 +95,13 @@ public class Prompt extends Activity implements OnClickListener{
 	public void onDestroy(){
 		s.safeShutdown();
 	}
+	
+	// here is the has function. it inputs the username and turns it into an integer.
+    private Integer userNameHash(String username){
+    	int id = 1;
+   /* 	for  ( int j = 0; j <= username.length() - 1; j++){
+    		  id= id*31+username.charAt(j);
+   		}
+    */ 	return id;
+    }
 }
