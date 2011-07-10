96ad5f59e3cc964f0bef9c46eb23fbfdaa73b64e
ttkirkland <tkirk@brandeis.edu>
2011-07-09 21:09:01 -0400

    cleaned code

--- src/org/jbs/happysad/Chart.java
+++ src/org/jbs/happysad/Chart.java
@@ -14,116 +14,88 @@ import android.content.Intent;
 import android.graphics.Color;
 
 /**
- * Sales growth demo chart.
+ * First HappyData Chart
+ * @author HappySad
  */
-public class Chart extends AbstractDemoChart {
+public class Chart extends AbstractChart {
   	
 	/**
-   * Returns the chart name.
-   * 
-   * @return the chart name
-   */
-  public String getName() {
-    return "Happy - Sad";
-  }
-
-  /**
-   * Returns the chart description.
-   * 
-   * @return the chart description
-   */
-  public String getDesc() {
-    return "This chart reflects time (x axis) vs happiness (y axis)";
-  }
-
-  /**
-   * Executes the chart demo.
-   * 
-   * @param context the context
-   * @return the built intent
-   */
-  public Intent execute(Context context) {
-	
-	//aqui*.
-	HappyData datahelper = new HappyData(context);
-	ArrayList<HappyBottle> plottables = datahelper.getAllHistory();
-	
-	ArrayList<Double> cat = emoTrace(plottables);
-	
-	double [] dog = new double[cat.size()]; // = (double []) cat.toArray();
-	for (int i=0; i<cat.size(); i++) {
-		dog[i]=cat.get(i);
+	 * Returns the chart name as a string
+	 * @return the chart name as a string
+	 */
+	public String getName() {
+		return "Happy - Sad";
 	}
-	  
-	String[] titles = new String[] { "Follow the up's and down's" };
-    List<Date[]> dates = new ArrayList<Date[]>();
-    List<double[]> values = new ArrayList<double[]>();
-    Date[] dateValues = new Date[] { new Date(111, 0, 1), new Date(111, 3, 1), new Date(111, 6, 1),
-        new Date(111, 9, 1), new Date(112, 0, 1), new Date(112, 3, 1), new Date(112, 6, 1),
-        new Date(112, 9, 1), new Date(113, 0, 1), new Date(113, 3, 1), new Date(113, 6, 1),
-        new Date(113, 9, 1), new Date(114, 0, 1), new Date(114, 3, 1), new Date(114, 6, 1),
-        new Date(114, 9, 1), new Date(115, 0, 1), new Date(115, 3, 1), new Date(115, 6, 1),
-        new Date(115, 9, 1), new Date(116, 0, 1), new Date(116, 3, 1), new Date(116, 6, 1),
-        new Date(116, 9, 1), new Date(116, 11, 1) };
-    dates.add(dateValues);
 
-    values.add(dog);
-    
-    //values.add(new double[] { 4.9, 5.3, 3.2, 4.5, 6.5, 4.7, 5.8, 4.3, 4, 2.3, -0.5, -2.9, 3.2, 5.5,
-    //    4.6, 9.4, 4.3, 1.2, 0, 0.4, 4.5, 3.4, 4.5, 4.3, 4 });
-    int[] colors = new int[] { Color.CYAN };
-    PointStyle[] styles = new PointStyle[] { PointStyle.POINT };
-    XYMultipleSeriesRenderer renderer = buildRenderer(colors, styles);
-    setChartSettings(renderer, "Happy Chart", "Date", "Happiness", dateValues[0].getTime(),
-        dateValues[dateValues.length - 1].getTime(), -4, 11, Color.LTGRAY, Color.WHITE);
-    //renderer.setXLabels(12);
-    renderer.setYLabels(10);
-    //renderer.setShowGrid(true);
-    //renderer.setXLabelsAlign(Align.RIGHT);
-    //renderer.setYLabelsAlign(Align.RIGHT);
-    renderer.setZoomButtonsVisible(true);
-    //renderer.setPanLimits(new double[] { -10, 20, -10, 40 });
-    //renderer.setZoomLimits(new double[] { -10, 20, -10, 40 });
-    return ChartFactory.getTimeChartIntent(context, buildDateDataset(titles, dates, values),
-        renderer, "MMM yyyy");
-  }
+	/**
+	 * Returns the chart description.
+	 * @return the chart description
+	 */
+	public String getDesc() {
+		return "This chart reflects time (x axis) vs happiness (y axis)";
+	}
+
+	/**
+	 * Executes the chart demo.
+	 * @param context the context
+	 * @return the built intent
+	 */
+	public Intent execute(Context context) {
+		HappyData datahelper = new HappyData(context);
+		ArrayList<HappyBottle> plottables = datahelper.getAllHistory();
+		ArrayList<Double> cat = emoTrace(plottables);
+	
+		double [] dog = new double[cat.size()]; // = (double []) cat.toArray();
+		for (int i=0; i<cat.size(); i++)
+			dog[i]=cat.get(i);
+		String[] titles = new String[] { "Follow the up's and down's" };
+		List<Date[]> dates = new ArrayList<Date[]>();
+		List<double[]> values = new ArrayList<double[]>();
+		Date[] dateValues = new Date[] { new Date(111, 0, 1), new Date(111, 3, 1), new Date(111, 6, 1),
+				new Date(111, 9, 1), new Date(112, 0, 1), new Date(112, 3, 1), new Date(112, 6, 1),
+				new Date(112, 9, 1), new Date(113, 0, 1), new Date(113, 3, 1), new Date(113, 6, 1),
+				new Date(113, 9, 1), new Date(114, 0, 1), new Date(114, 3, 1), new Date(114, 6, 1),
+				new Date(114, 9, 1), new Date(115, 0, 1), new Date(115, 3, 1), new Date(115, 6, 1),
+				new Date(115, 9, 1), new Date(116, 0, 1), new Date(116, 3, 1), new Date(116, 6, 1),
+				new Date(116, 9, 1), new Date(116, 11, 1) };
+		dates.add(dateValues);
+
+		values.add(dog);
+		int[] colors = new int[] { Color.CYAN };
+		PointStyle[] styles = new PointStyle[] { PointStyle.POINT };
+		XYMultipleSeriesRenderer renderer = buildRenderer(colors, styles);
+		setChartSettings(renderer, "Happy Chart", "Date", "Happiness", dateValues[0].getTime(),
+				dateValues[dateValues.length - 1].getTime(), -4, 11, Color.LTGRAY, Color.WHITE);
+		renderer.setYLabels(10);
+		renderer.setZoomButtonsVisible(true);
+		return ChartFactory.getTimeChartIntent(context, buildDateDataset(titles, dates, values),
+				renderer, "MMM yyyy");
+	}
   
-  //aqui*.
-  public ArrayList<Double> emoTrace(ArrayList<HappyBottle> plottables){
-	   Iterator<HappyBottle> itr = plottables.iterator(); 
-	   double trace = 0;
-	   ArrayList<Double> traceline = new ArrayList<Double>();
-	   traceline.add(trace);
-	   while(itr.hasNext()) {
-		     
+	public ArrayList<Double> emoTrace(ArrayList<HappyBottle> plottables){
+		Iterator<HappyBottle> itr = plottables.iterator(); 
+		double trace = 0;
+		ArrayList<Double> traceline = new ArrayList<Double>();
+		traceline.add(trace);
+		while(itr.hasNext()) {     
 		   	HappyBottle element = itr.next();
-		     
 		     if (element.getEmo() == 1){
 		    	trace += 2; 
 		    	traceline.add(trace); 
-		    	
 		     } else {
 		    	trace -= 2; 
 			    traceline.add(trace); 
 		     }
-	   } 
-	   
-	   return traceline;
-  }
-  
-  //aqui*.
-  public ArrayList<Date> dateTrace(ArrayList<HappyBottle> plottables){
-	   Iterator<HappyBottle> itr = plottables.iterator(); 
-	   ArrayList<Date> dates = new ArrayList<Date>();
-	   while(itr.hasNext()) {
-		     
+		} 
+		return traceline;
+	}
+	
+	public ArrayList<Date> dateTrace(ArrayList<HappyBottle> plottables){
+		Iterator<HappyBottle> itr = plottables.iterator(); 
+		ArrayList<Date> dates = new ArrayList<Date>();
+		while(itr.hasNext()) {     
 		   	HappyBottle element = itr.next();
-		    
-		   	//aqui**.
-		    //dates.add(element.getTime());
 	   } 
-	   
 	   return dates;
-  }
-
-}
+	}
+}	
