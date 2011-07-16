8f16b19db9588f15301c789fa9c590df3782e1ec
ttkirkland <tkirk@brandeis.edu>
2011-07-16 18:16:24 -0400

    removed refresh button

--- src/org/jbs/happysad/History.java
+++ src/org/jbs/happysad/History.java
@@ -6,43 +6,30 @@ import java.util.HashMap;
 import android.app.ListActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.SimpleAdapter;
 
-//this activity corresponds to the "mytrack" activity
-//it's a nice text-based list of updates, sorted by date created.
-public class History extends ListActivity implements OnClickListener{
+/**
+ * Creates a text-based list of a user's updates, sorted by date created
+ * @author HappySad
+ */
+public class History extends ListActivity {
 	private HappyData dataHelper;
-	private static int[] TO = {R.id.item_text1, R.id.item_text2 };
+	private static int[] TO = {R.id.item_text1, R.id.item_text2};
 	private static String[] FROM = { "line1","line2" };	
 	private SimpleAdapter adapter;
 	StringBuilder result;
-
+	
+	/**
+	 * Initializes Activity
+	 */
 	@Override
 	public void onCreate(Bundle savedInstanceState){
-		//set up the oncreate. simple
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.history);
 
-		View refreshButton = findViewById(R.id.refresh_button);
-    	refreshButton.setOnClickListener(this);
-
 		dataHelper = new HappyData(this);
-		//buttons etc.
 		ArrayList<HappyBottle> list = dataHelper.getMyHistory(); 
 		showUpdates(list);
-	}
-
-	public void onClick(View v) {
-		switch(v.getId()) {		
-		//this refresh button requeries the database for you to see if you have new updates
-		case R.id.refresh_button:	
-			ArrayList<HappyBottle> list = dataHelper.getMyHistory(); 
-			showUpdates(list);
-			adapter.notifyDataSetChanged();
-			break;
-		}
 	}
 
 	private void showUpdates(ArrayList<HappyBottle> l){
