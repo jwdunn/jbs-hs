bd93d62a9291b2d3ca88675f37e7d918e40df3fe
Jared Dunn <jwdunn1@brandeis.edu>
2011-07-16 21:22:26 -0400

    user accounts

--- src/org/jbs/happysad/More.java
+++ src/org/jbs/happysad/More.java
@@ -4,8 +4,6 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.graphics.Matrix;
-import android.graphics.PointF;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -14,19 +12,12 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.WindowManager;
 import android.widget.TextView;
-//import android.util.Log;
 import android.widget.Toast;
-
-
 /**
  * Creates the More activity
- * @author HS
+ * @author HappyTrack
  */
-
 public class More extends Activity implements OnClickListener {
-	//for debugging purposes, delete after debugging.
-	//private static final String TAG = "there's more screen";
-
 	//fields
 	private LocationManager gpsLocationManager;
 	private LocationManager networkLocationManager;
@@ -39,23 +30,16 @@ public class More extends Activity implements OnClickListener {
 	private HappyData dataHelper;
 	short emotion = -1;
 	String extradata;
+
 	private UIDhelper UIDh;
 	private long myID;
-	String shareString = "";
 
-	Matrix matrix = new Matrix();
-	Matrix savedMatrix = new Matrix();
 	
-	// We can be in one of these 3 states for the zooming
-	static final int NONE = 0;
-	static final int DRAG = 1;
-	static final int ZOOM = 2;
-	int mode = NONE;
 	
-	// Remember some things for zooming
-	PointF start = new PointF();
-	PointF mid = new PointF();
-	float oldDist = 1f;
+
+
+   
+
 
 	/**
 	 * Initializes activity
@@ -78,18 +62,10 @@ public class More extends Activity implements OnClickListener {
 		//prevent text edit from being focused onCreate
 		getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
 		
-
-		//this creates the on touch listenter for the photo button
-		View buttonImageCapture = (View) findViewById(R.id.camera_button);
-		buttonImageCapture.setOnClickListener(this);
-		
-		//Finds the share_button view
-		View shareButton = findViewById(R.id.share);
-		shareButton.setOnClickListener(this);
-
 		//Finds the submit_button view
 		View submitButton = findViewById(R.id.more_to_dash);
 		submitButton.setOnClickListener(this);
+
 		
 		 UIDh = new UIDhelper();
 		 myID =UIDh.getUID();
@@ -113,45 +89,13 @@ public class More extends Activity implements OnClickListener {
 				toast.show();
 			}
 			break;
-		case R.id.camera_button:
-			Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-			startActivityForResult(intent, 0);
-			break;	
-		case R.id.share:
-			if (emotion == 1){
-				shareString = ((TextView) findViewById(R.id.more_textbox)).getText().toString();
-				share("Happy", shareString);
-			}
-			else{
-				shareString = ((TextView) findViewById(R.id.more_textbox)).getText().toString();
-				share("Sad", shareString);
-			}
-			break;	
 		}
-	} 
-
-	/**
-	 * Given an emotion and the message that goes with it, share it with others using the various servics (facebook twitter etc) 
-	 * @param emo
-	 * @param text
-	 */
-	public void share(String subject,String text) {
-		final Intent intent = new Intent(Intent.ACTION_SEND);
-
-		intent.setType("text/plain");
-		intent.putExtra(Intent.EXTRA_SUBJECT, subject);
-		intent.putExtra(Intent.EXTRA_TEXT, text);
-
-		startActivity(Intent.createChooser(intent, getString(R.string.share)));
 	}
-
-
-
+	
 	/**
 	 * Helper method to deal with location.
 	 */
 	private void locationStuff(){
-
 		// Acquire a reference to the system Location Manager
 		gpsLocationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
 		networkLocationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
@@ -199,10 +143,14 @@ public class More extends Activity implements OnClickListener {
 			makeUseOfNewLocation(locationGPS);
 			updateNetworkLocation(locationNetwork);
 
-		}
-		catch (Exception e){
-		}
-
+		} catch (Exception e){}
+	}
+	
+	private void removeLocation() {
+		gpsLocationManager.removeUpdates(gpsLocationListener);
+		networkLocationManager.removeUpdates(networkLocationListener);
+		gpsLocationManager = null;
+		networkLocationManager = null;
 	}
 
 	/**
@@ -210,12 +158,10 @@ public class More extends Activity implements OnClickListener {
 	 * @param location
 	 */
 	private void makeUseOfNewLocation(Location location) {		
-
 		if(GPS_longitude == 0 && GPS_latitude == 0){
 			GPS_latitude = (int) (location.getLatitude()*1E6);
 			GPS_longitude = (int) (location.getLongitude()*1E6);
 		}
-
 	}
 
 	/**
@@ -238,28 +184,19 @@ public class More extends Activity implements OnClickListener {
 			GPS_longitude = Network_longitude;
 			GPS_latitude = Network_latitude;
 		}
-
 		HappyBottle b = new HappyBottle(myID, GPS_latitude, GPS_longitude, emotion, msg, System.currentTimeMillis());
 		dataHelper = new HappyData(this);
 		dataHelper.addBottle(b);
 	}
 
-
-	/**
-	 * Disables GPS Managers and Listeners
-	 */
-	public void onPause() {
+	//Disables GPS Managers and Listeners
+	protected void onPause() {
 		super.onPause();
-		gpsLocationManager.removeUpdates(gpsLocationListener);
-		networkLocationManager.removeUpdates(networkLocationListener);
-		gpsLocationManager = null;
-		networkLocationManager = null;
+		removeLocation();
 	}
 
-	/**
-	 * Enables GPS Managers and Listeners
-	 */
-	public void onResume() {
+	//Enables GPS Managers and Listeners
+	protected void onResume() {
 		super.onResume();
 		locationStuff();
 	}
