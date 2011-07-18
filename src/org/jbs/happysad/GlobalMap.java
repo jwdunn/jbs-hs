34a3a870cdcea8325b1cf81a409d52bc65d88e99
Sahar Massachi <sayhar@gmail.com>
2011-07-18 01:02:14 -0400

    Much more advanced map. On-The-Fly, it calls the server to find the 100 most recent bottles that are in the rectangle of map shown, but only when you move or zoom the view.
    Handlers queue up these calls, so only the most recent is displayed
    (to save time!).
    
    Change-Id: Iddedf756fa83f470307aaf7358d5bde3f92fe73b

--- src/org/jbs/happysad/GlobalMap.java
+++ src/org/jbs/happysad/GlobalMap.java
@@ -1,9 +1,12 @@
+//my failed java part 2 (sunday)
+
 package org.jbs.happysad;
 
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -14,6 +17,7 @@ import com.google.android.maps.MapView;
 import com.google.android.maps.MyLocationOverlay;
 import com.google.android.maps.OverlayItem;
 import android.widget.Button;
+import android.widget.Toast;
 
 import java.sql.Timestamp;
 import java.util.ArrayList;
@@ -24,6 +28,7 @@ import java.util.ArrayList;
  */
 public class GlobalMap extends MapActivity implements OnClickListener {
 	//fields
+	private static final String TAG = "GlobalMap";
 	private MapView map;
 	private MapController controller;
 	int checkHappy = 1;
@@ -32,10 +37,13 @@ public class GlobalMap extends MapActivity implements OnClickListener {
 	ItemizedEmotionOverlay happyOverlay; 
 	ItemizedEmotionOverlay sadOverlay;
 	HappyData datahelper = new HappyData(this);
-	ArrayList<HappyBottle> plottables;
-	int zoomLevel = -1;
-	GeoPoint center = new GeoPoint(0,0);
-	
+	//	private volatile ArrayList<HappyBottle>  plottables;
+	int zoomLevel;
+	GeoPoint center;
+	private Handler handler;
+	Runnable latestThread;
+	ZoomPanListener zpl;
+
 	/**
 	 * Initializes Activity
 	 */
@@ -43,48 +51,52 @@ public class GlobalMap extends MapActivity implements OnClickListener {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.map);
-		
+
 		//Defines the drawable items for the happy and sad overlays
 		Drawable happyface = this.getResources().getDrawable(R.drawable.pinhappy);
 		Drawable sadface = this.getResources().getDrawable(R.drawable.pinsad);
-		
+
 		//initializes the happy and sad overlays
 		happyOverlay = new ItemizedEmotionOverlay(happyface, this);	
 		sadOverlay = new ItemizedEmotionOverlay(sadface, this);
-		
+
 		//initialize and display map view and user location
 		initMapView();
 		initMyLocation();	
-		//updateToView();
-		
+
 		//adds items to overlays
 		//emotionOverlayFiller(1,plottables,happyOverlay);
 		//emotionOverlayFiller(0,plottables,sadOverlay);
-		
+
 		//Finds the show_sad view
 		View sadButton = findViewById(R.id.showSad);
 		sadButton.setOnClickListener(this);
-		
+
 		//Finds the show_happy view
 		View happyButton = findViewById(R.id.showHappy);
 		happyButton.setOnClickListener(this); 
-		
+
 		//Finds the switch_view
 		View switchButton = findViewById(R.id.switchView);
 		switchButton.setOnClickListener(this); 
-		
+
 		//Finds the chart_button view
 		View chartButton = findViewById(R.id.myTrack_button);
 		chartButton.setOnClickListener(this);  	
-		
+
 		//Finds the history_button view
 		View histButton = findViewById(R.id.myChart_button);
 		histButton.setOnClickListener(this);
-		
+
 		//Finds the my_map view
 		View myButton = findViewById(R.id.map);
 		((Button) myButton).setText("MyMap");
 		myButton.setOnClickListener(this);
+
+		center = new GeoPoint(0,0);
+		zoomLevel = -1;
+		handler = new Handler();
+
 	}
 
 	/**
@@ -92,14 +104,9 @@ public class GlobalMap extends MapActivity implements OnClickListener {
 	 */
 	@Override
 	public void onClick(View v) {
-		
-		//updateToView();
-		//adds items to overlays
-		//emotionOverlayFiller(1,plottables,happyOverlay);
-		//emotionOverlayFiller(0,plottables,sadOverlay);
-		
+
 		switch(v.getId()){
-		
+
 		//checks what current view is, then switches it off and starts the alternate view
 		case R.id.switchView:
 			if (map.isStreetView()==false){
@@ -110,8 +117,8 @@ public class GlobalMap extends MapActivity implements OnClickListener {
 				map.setSatellite(true);
 			}
 			break;
-		
-		//used to show/hide the happy faces
+
+			//used to show/hide the happy faces
 		case R.id.showHappy:
 			if (checkHappy==0){ 
 				map.getOverlays().add(happyOverlay); //adds happy face overlay to visible overlays 
@@ -125,14 +132,14 @@ public class GlobalMap extends MapActivity implements OnClickListener {
 				newOverlay(); //method call
 			}
 			break;
-		
-		//used to show/hide the sad faces
+
+			//used to show/hide the sad faces
 		case R.id.showSad:		
 			if (checkSad == 0){
 				map.getOverlays().add(sadOverlay); //adds sad face overlay to visible overlays
 				checkSad = 1; 
-				} else{
-					map.getOverlays().clear(); //clears all overlays
+			} else{
+				map.getOverlays().clear(); //clears all overlays
 				if (this.checkHappy==1){
 					map.getOverlays().add(happyOverlay); //if happy faces should be visible, it adds them back
 				}
@@ -140,23 +147,26 @@ public class GlobalMap extends MapActivity implements OnClickListener {
 				newOverlay(); //method call
 			}
 			break;
-		
+
 		case R.id.map:
 			startActivity(new Intent(this, MyMap.class));
 			break;
-		
+
 		case R.id.myTrack_button:
 			startActivity(new Intent(this, History.class));
 			break;
+		case R.id.myChart_button:
+			break;
 		}
+
 		map.invalidate();
 	}
-	
+
 	//helper method for showHappy and showSad onClick cases
 	private void newOverlay() {
 		map.getOverlays().add(userLocationOverlay);
 	}
-	
+
 	//Finds and initializes the map view.
 	private void initMapView() {
 		map = (MapView) findViewById(R.id.themap);
@@ -167,7 +177,7 @@ public class GlobalMap extends MapActivity implements OnClickListener {
 		map.getOverlays().add(happyOverlay);
 		map.setBuiltInZoomControls(false); //hides the default map zoom buttons so they don't interfere with the app buttons
 	}
-	
+
 	//Starts tracking the users position on the map. 
 	private void initMyLocation() {
 		userLocationOverlay = new MyLocationOverlay(this, map);
@@ -181,26 +191,29 @@ public class GlobalMap extends MapActivity implements OnClickListener {
 		});
 		map.getOverlays().add(userLocationOverlay); //adds the users location overlay to the overlays being displayed
 	}
-	
-	
+
+
 	//creates an emotion overlay
-	private static void emotionOverlayFiller(int emotion, ArrayList<HappyBottle> plottables, ItemizedEmotionOverlay itemizedoverlay){ 
-		for(int i = 0; i<plottables.size();i++) {
-			HappyBottle element = plottables.get(i);
-			if (element.getEmo()==emotion) { //happy or sad filter
-				int latitude =  element.getLat();
-				int longitude = element.getLong();
+	private static synchronized void emotionOverlaySetter(int emotion, ArrayList<HappyBottle> toshow, ItemizedEmotionOverlay overlay){ 
+
+		if (toshow == null) {return; }///THIS IS A PROBLEM AND SHOULD NEVER HAPPEN
+		overlay.emptyOverlay();
+		for(HappyBottle bottle : toshow) {
+
+			if (bottle.getEmo()==emotion) { //happy or sad filter
+				int latitude = bottle.getLat();
+				int longitude = bottle.getLong();
 				GeoPoint point = new GeoPoint(latitude,longitude);
-				String S = (String) new Timestamp(element.getTime()).toLocaleString();
-				itemizedoverlay.addToOverlay(new OverlayItem(point, S+emotion, element.getMsg()));
+				String S = (String) new Timestamp(bottle.getTime()).toLocaleString();
+				overlay.addToOverlay(new OverlayItem(point, S+emotion, bottle.getMsg()));
 			}
 		}
 	}
-	
+
 	/**
 	 * This method updates the overlays for only the current the current view
 	 */
-	private void updateToView(){
+	private ArrayList<HappyBottle> updateToView(){
 		//Log.w("updateToView", "ERROR in new method");
 		GeoPoint center = map.getMapCenter(); //gets coordinates for map view's center
 		int centerLat = center.getLatitudeE6(); //finds center's latitude
@@ -211,81 +224,108 @@ public class GlobalMap extends MapActivity implements OnClickListener {
 		int maxLong = centerLong+width/2; //gets the right most longitude shown
 		int maxLat = centerLat+height/2; //gets the top most latitude shown
 		int minLat = centerLat-height/2; //gets the bottom most latitude shown
-		plottables = datahelper.getLocalRecent(minLat,maxLat,minLong,maxLong,100);
+		Log.d(TAG, "we are now using local pins with updateToView");
+		return datahelper.getLocalRecent(minLat,maxLat,minLong,maxLong,100);
 	}
 
 	//to sync and update bottles with mapview
-	private void updater(GeoPoint center, int zoomLevel){
-		if(!(map.getMapCenter() == center && map.getZoomLevel()==zoomLevel)){
-			this.center = map.getMapCenter();
-			this.zoomLevel = map.getZoomLevel();
-			updateToView();
+	private ArrayList<HappyBottle> updater(){
+		if(isMoved()){
+			Log.d(TAG, "updatetoview from updater");
+			return updateToView();
 		}
+		return null;
 	}
-	
-	class Async extends AsyncTask <MapView, Integer, Boolean>{
-		//standard methods you have to implement to use AsyncTask
-		protected void onProgressUpdate(Integer... progress) {
-			//logo.log("async.onProgressUpdate");
-		}
-		//standard methods you have to implement to use AsyncTask
-		protected void onPostExecute(Long result) {
-			//logo.log("async.onProgressUpdate");
-		}
-		//this is the method that runs in background and will be very useful
+
+	private void startStabilizeLatLong(){
+
+		Runnable runnable = new Runnable(){
+			@Override
+			public void run(){
+				int maxcount = 1000;
+				for (int i = 0; i < maxcount; i++) {
+					//testing about getLatitudeSpan and getLongitudeSpan fake values
+					if (map.getLatitudeSpan() == 0 & map.getLongitudeSpan() == 360000000){
+						try{
+							//thread will sleep awhile!
+							Thread.sleep(80);
+						}catch (InterruptedException ex){
+							ex.printStackTrace();
+						}}
+					else {//OK that means the map has stabilized and if we get height and width will have be real values. 
+						handler.removeCallbacks(latestThread); //if you have another of these tasks queued for the handler, remove it. 
+						latestThread = new Runnable(){
+							@Override
+							public void run(){
+								Log.d(TAG, "running the thread that updates the overlays");
+								ArrayList<HappyBottle> newBottles = updater();
+								if (newBottles != null){
+									emotionOverlaySetter(1,newBottles,happyOverlay);
+									emotionOverlaySetter(0,newBottles,sadOverlay);
+								}
+							}
+						};
+						handler.postDelayed(latestThread, 10); //delay the posing of the new pins by a tiny fraction of a  second, because that way it will let you invalidate if you keep moving.
+						maxcount = 0; //we are sure to exit this infernal loop
+						return;
+					}	};}};
+
+					new Thread(runnable).start();
+
+	}
+
+	private class ZoomPanListener extends AsyncTask<Void, Void, Void>{
 		@Override
-		protected Boolean doInBackground(MapView... params) {
-			//I defined a max bound for cycle time.... every cycle must have a way out condition!
-			int maxcount = 100;
-			//It takes maps from parameters
-			MapView mappa = params[0];
-			//every time it runs
-			for (int i = 0; i < maxcount; i++) {
-				//testing about getLatitudeSpan and getLongitudeSpan fake values
-				if (mappa.getLatitudeSpan() == 0 & mappa.getLongitudeSpan() == 360000000)
-				{
-					try
-					{
-						// fake values, thread will sleep awhile!
-						Thread.sleep(80);
-					}
-					catch (InterruptedException ex)
-					{
-						Log.e("async.doInBackground. " + ex.getMessage(), null);
-					}
+		protected Void doInBackground(Void... params) {
+			while(true){
+				if(isMoved()){
+					startStabilizeLatLong();
 				}
-				else //good values, we can use them 
-				{
-					updater(center,zoomLevel);
-					happyOverlay.emptyOverlay();
-					sadOverlay.emptyOverlay();
-					emotionOverlayFiller(1,plottables,happyOverlay);
-					emotionOverlayFiller(0,plottables,sadOverlay);
-					return true;
-				}
-			}
-			return false;
-		}
-	}
-	
+				try {
+					Thread.sleep(100);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}}}}
+
 	//Required method to make the map work
 	protected boolean isRouteDisplayed() {
 		return false;
 	}
-	
+
+	public boolean isMoved() {
+		GeoPoint trueCenter =map.getMapCenter();
+		int trueZoom = map.getZoomLevel();
+
+		if(!((trueCenter.equals(center)) && (trueZoom == zoomLevel))){
+			center = map.getMapCenter();
+			zoomLevel = map.getZoomLevel();
+			Log.d(TAG, "You moved!:" + center.toString() + " zoom: " + zoomLevel);
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+
 	//Disables MyLocation
 	@Override
 	protected void onPause() {
-		super.onPause();
 		userLocationOverlay.disableMyLocation();  
+		synchronized (zpl){
+			zpl.cancel(true);}
+		super.onPause();
 	}
-	
-	//Enables MyLocation
+
+	//Enables MyLocation 
 	@Override
 	protected void onResume() {
 		super.onResume();
 		userLocationOverlay.enableMyLocation();
-		Async sync = new Async();
-		sync.execute(map);
+		zpl = new ZoomPanListener();
+		zpl.execute(null);
+		center = new GeoPoint(-1,-1); //fake a move so that updater thinks we've moved and populates the initial screen.
+		startStabilizeLatLong();
+		
+		
 	}
 }
